76e4a29e2163fd6601907b39f7fae905c2939336
znt10 <jc970807@gmail.com>
2025-09-07 14:29:23 -0300

    login

---
+++ projeto_map/artistas/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import redirect, render
+from django.views import View
+from .models import Perfil
+
+
+class LoginView(View):
+    def get(self,request):
+        return render(request, 'login.html')
+    def post(self,request):
+        # Lógica de autenticação aqui
+        email = request.POST.get('username')
+        senha = request.POST.get('password')
+
+        if not email or not senha:
+            return render(request, 'login.html', {'error': 'Preencha todos os campos.'})
+
+        if Perfil.objects.filter(email=email).exists():
+            return render(request, 'login.html', {'error': 'email já cadastrado.'})
+        
+        Perfil.objects.create(email=email, senha=senha)
+
+
+        return redirect('sim')
+
+
+class sim(View):
+    def get(self,request):
+        return render(request, 'sim.html')
+
--- projeto_map/projeto_map/settings.py
+++ projeto_map/projeto_map/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'artistas',
 ]
 
 MIDDLEWARE = [
@@ -110,21 +111,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
